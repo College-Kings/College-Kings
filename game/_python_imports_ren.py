4562482e25c2dd3c80aa50cedbd66e2aa8923dea
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-04-25 10:17:58 +0100

    fixed #316

---
+++ game/_python_imports_ren.py
@@ -0,0 +1,17 @@
+"""renpy
+python early:
+"""
+
+from __future__ import annotations
+import datetime
+import enum
+import math
+import os
+import random
+import re
+import requests
+import uuid
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from enum import Enum, IntEnum, Flag
+from typing import Any, Callable, Optional, Union
