d5754ef68c5e28808ab6774c41676781c2e073ee
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-04-28 19:47:45 +0100

    added correct girl relationships

--- game/after_default_ren.py
+++ game/after_default_ren.py
@@ -1,5 +1,9 @@
 from renpy import store, config
-from renpy.exports import ToggleVariable, If, Function
+from renpy.exports import ToggleVariable
+
+from game.characters.Relationship_ren import Relationship
+from game.characters.Frat_ren import Frat
+from game.reputation.Reputations_ren import Reputations
 
 """renpy
 init python:
@@ -16,7 +20,7 @@ def add_applications_to_phone() -> None:
     )
 
 
-def create_pb_option_variables() -> None:
+def create_pb_variables() -> None:
     store.pb_extra_options = (
         ("Took Chloe to the homecoming date", ToggleVariable("hcGirl", "chloe")),
         ("Took Riley to the homecoming date", ToggleVariable("hcGirl", "riley")),
@@ -31,9 +35,88 @@ def create_pb_option_variables() -> None:
         ("Invite Emily to Europe", ToggleVariable("emily_europe")),
     )
 
+    store.pb_girls = (
+        (
+            store.amber,
+            None,
+            Reputations.POPULAR,
+            (Relationship.FRIEND, Relationship.KISSED, Relationship.FWB),
+        ),
+        (
+            store.aubrey,
+            None,
+            Reputations.POPULAR,
+            (Relationship.FRIEND, Relationship.FWB),
+        ),
+        (
+            store.chloe,
+            None,
+            Reputations.POPULAR,
+            (
+                Relationship.FRIEND,
+                Relationship.FWB,
+                Relationship.GIRLFRIEND,
+            ),
+        ),
+        (store.emily, None, Reputations.LOYAL, (Relationship.FRIEND, Relationship.FWB)),
+        (
+            store.lauren,
+            None,
+            Reputations.LOYAL,
+            (
+                Relationship.FRIEND,
+                Relationship.KISSED,
+                Relationship.GIRLFRIEND,
+            ),
+        ),
+        (
+            store.lindsey,
+            None,
+            Reputations.POPULAR,
+            (Relationship.FRIEND, Relationship.KISSED, Relationship.FWB),
+        ),
+        (
+            store.ms_rose,
+            Frat.WOLVES,
+            Reputations.CONFIDENT,
+            (Relationship.FRIEND, Relationship.KISSED, Relationship.FWB),
+        ),
+        (
+            store.nora,
+            None,
+            Reputations.CONFIDENT,
+            (
+                Relationship.FRIEND,
+                Relationship.FWB,
+            ),
+        ),
+        (
+            store.penelope,
+            None,
+            Reputations.CONFIDENT,
+            (
+                Relationship.FRIEND,
+                Relationship.DATING,
+            ),
+        ),
+        (
+            store.riley,
+            None,
+            Reputations.CONFIDENT,
+            (Relationship.FRIEND, Relationship.KISSED, Relationship.FWB),
+        ),
+        (
+            store.samantha,
+            Frat.APES,
+            Reputations.LOYAL,
+            (Relationship.FRIEND, Relationship.KISSED, Relationship.FWB),
+        ),
+    )
 
+
+store.pb_girls = ()
 store.pb_extra_options = ()
 store.pb_advanced_options = ()
 
 config.after_default_callbacks.append(add_applications_to_phone)
-config.after_default_callbacks.append(create_pb_option_variables)
+config.after_default_callbacks.append(create_pb_variables)
